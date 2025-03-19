80e117c57bcf4d57abbd83c77799805285c35eee
KingStar0008 <shivank.mishra1@tothenew.com>
2025-03-19 12:09:55 +0530

    Assert Update

--- src/main/java/org/amazon/Dashboard.java
+++ src/main/java/org/amazon/Dashboard.java
@@ -2,6 +2,8 @@ package org.amazon;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 public class Dashboard {
     private WebDriver driver;
@@ -10,8 +12,16 @@ public class Dashboard {
         this.driver = driver;
     }
 
+    public void DashboardAssert(){
+        WebElement openDashboard = driver.findElement(By.xpath("//a[@id='nav-logo-sprites']"));
+        String amazonText = openDashboard.getAttribute("aria-label");
+        Assert.assertTrue(openDashboard.isDisplayed(), "You are on amazon.com. You can also shop on Amazon India for millions of products with fast local delivery.");
+    }
+
     public void searchBox(String item){
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys(item);
         driver.findElement(By.id("nav-search-submit-button")).click();
     }
+
+
 }
--- src/test/java/com/amazonTest/DashboardTest.java
+++ src/test/java/com/amazonTest/DashboardTest.java
@@ -8,7 +8,9 @@ public class DashboardTest extends Base{
     public void dashboardPage(){
         getDriver().get("https://www.amazon.in/");
         Dashboard dashboard = new Dashboard(getDriver());
+        dashboard.DashboardAssert();
         dashboard.searchBox("Refrigerator");
+
     }
 
 }
